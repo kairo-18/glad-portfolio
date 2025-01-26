2f62e69802232d60e81f30cb1de7df3b8776e6ba
kairo-18 <gladimirdungo@gmail.com>
2025-01-26 23:32:22 +0800

    changed jpg to all caps

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const navbar = () => {
     const [showMenu, setShowMenu] = useState(false);
 return (
     <div className='w-full h-full flex text-white p-5 justify-between items-center'>
-            <div className="logo font-bold ml-[10%] flex"><img src='src/assets/logo.png' className='w-9 h-9 mt-[-7px]'/> Glad</div>
+            <div className="logo font-bold ml-[10%] flex"><img src='./src/assets/logo.png' className='w-9 h-9 mt-[-7px]'/> Glad</div>
             <ul className="hidden list-style-none gap-12 mr-[10%] sm:flex">
                     <li className='hover:transform hover:translate-0.5' ><a href="#home"><span className='text-[#C778DD] font-semibold'>/</span>home</a></li>
                     <li><a href="#about"><span className='text-[#C778DD] font-semibold'>/</span>about</a></li>
@@ -21,15 +21,15 @@ return (
                     </ul>
             )}
             <div id="hamburger-menu" className='sm:hidden absolute right-[5%]'>
-                <img src="/src/assets/bars-menu.png" className='w-6 transform active:scale-120' onClick={() => setShowMenu(!showMenu)}/>
+                <img src="./src/assets/bars-menu.png" className='w-6 transform active:scale-120' onClick={() => setShowMenu(!showMenu)}/>
             </div>
 
             <div className="absolute top-0 left-[4%]">
                 <div className='ml-[15px] w-[1px] h-[200px] bg-gray-300'></div>
                 <div className="mt-3">
-                    <img src='/src/assets/Github.png' className=''/>
-                    <img src='/src/assets/Email.png' className=''/>
-                    <img src='/src/assets/Linkedin.png' className=''/>
+                    <img src='./src/assets/Github.png' className=''/>
+                    <img src='./src/assets/Email.png' className=''/>
+                    <img src='./src/assets/Linkedin.png' className=''/>
                 </div>
             </div>
     </div>
